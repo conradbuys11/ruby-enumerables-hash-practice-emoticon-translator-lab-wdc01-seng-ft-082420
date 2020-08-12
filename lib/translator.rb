e4961211d46ba70b8402c01f32eb482db6b5eaba
Conrad Buys <conradbuys11@gmail.com>
2020-08-12 22:33:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,9 +3,10 @@ require "yaml"
 
 def load_library(yml_file)
   emoticon_hash = YAML.load_file(yml_file)
+  emoticon_hash
 end
 
-def get_japanese_emoticon(yml_file,jp_emote)
+def get_japanese_emoticon(yml_file, jp_emote)
   # code goes here
 end
 
