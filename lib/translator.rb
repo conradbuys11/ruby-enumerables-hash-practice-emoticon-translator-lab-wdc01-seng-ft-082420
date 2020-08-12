9f0b021e860b4903ab368362222db8e48464b59d
Conrad Buys <conradbuys11@gmail.com>
2020-08-12 22:48:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -33,9 +33,10 @@ def get_english_meaning(yml_file, jp_emote)
   #this is basically the same as get_japanese_emoticon but instead of returning the other emote, we're gonna return the name of the category
   
   load_library(yml_file).each do |emotion, emotes|
-    if emotes.any?(jp_emote)
+    
+    if emotes[:japanese] == jp_emote
       return emotion.to_s
     end
+    
   end
-  
 end
